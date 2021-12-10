dfa8c1945cc34876340e60633bf78c22ee87a7de
Ismael Peccin <ismaelpeccin@gmail.com>
2021-12-10 19:29:11 -0300

    Tela de explorar pronta

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,15 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
-function Explore() {
+function Explore({ history }) {
   return (
     <div>
       <Header />
-      Explorar
+      <button
+        type="button"
+        data-testid="explore-food"
+        onClick={ () => history.push('/explorar/comidas') }
+      >
+        Explorar Comidas
+      </button>
+      <button
+        type="button"
+        data-testid="explore-drinks"
+        onClick={ () => history.push('/explorar/bebidas') }
+      >
+        Explorar Bebidas
+      </button>
       <Footer />
     </div>
   );
 }
+
+Explore.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Explore;
