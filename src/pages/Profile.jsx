fcf1de1b520c442e6be6d434f5737352148f7733
Ismael Peccin <ismaelpeccin@gmail.com>
2021-12-27 16:20:46 -0300

    Tela de perfil

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,13 +1,57 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Footer from '../components/Footer';
 
-function Profile() {
+function Profile({ history }) {
+  const user = localStorage.getItem('user');
   return (
     <div data-testid="page-title">
-      Perfil
+      <h2 data-testid="profile-email">
+        { user }
+      </h2>
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+        onClick={
+          (() => {
+            history.push('/receitas-feitas');
+          })
+        }
+      >
+        Receitas Feitas
+      </button>
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+        onClick={
+          (() => {
+            history.push('/receitas-favoritas');
+          })
+        }
+      >
+        Receitas Favoritas
+      </button>
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+        onClick={
+          (() => {
+            localStorage.clear();
+            history.push('/');
+          })
+        }
+      >
+        Sair
+      </button>
       <Footer />
     </div>
   );
 }
+
+Profile.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Profile;
