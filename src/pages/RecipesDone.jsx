5d2c73b133de055d22eae4387b7af7809a16be6e
Breno da Cunha Campanha <comosaberonline@gmail.com>
2022-01-10 20:32:45 -0400

    Tela de recipesDone Feita!

--- src/pages/RecipesDone.jsx
+++ src/pages/RecipesDone.jsx
@@ -1,34 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import DoneMeals from '../components/DoneMeals';
-import DoneDrinks from '../components/DoneDrinks';
-
-// const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
+import { checkIfThereIsLocalStorage } from '../services/supportFunctions';
 
 function RecipesDone() {
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    setMeals(checkIfThereIsLocalStorage('doneRecipes'));
+  }, []);
+
+  const arrayToRender = ({ name }) => {
+    const typeOfFilter = {
+      Food: 'comida',
+      Drinks: 'bebida',
+    };
+    if (name !== 'All') {
+      const filteredArray = meals.filter((item) => item.type === typeOfFilter[name]);
+      return setMeals(filteredArray);
+    }
+    setMeals(checkIfThereIsLocalStorage('doneRecipes'));
+  };
+
   return (
     <div>
       <Header pageName="Receitas Feitas" />
       <button
         type="button"
         data-testid="filter-by-all-btn"
+        name="All"
+        onClick={ ({ target }) => arrayToRender(target) }
       >
         All
       </button>
       <button
         type="button"
         data-testid="filter-by-food-btn"
+        name="Food"
+        onClick={ ({ target }) => arrayToRender(target) }
       >
         Food
       </button>
       <button
         type="button"
         data-testid="filter-by-drink-btn"
+        name="Drinks"
+        onClick={ ({ target }) => arrayToRender(target) }
       >
         Drinks
       </button>
-      <DoneMeals />
-      <DoneDrinks />
+      <DoneMeals meals={ meals } />
     </div>
   );
 }
