185da9107dbe5919d0c8e2eb21269ed5cda70b0c
William Schmidt <9williamschmidt@gmail.com>
2022-01-11 19:51:25 -0300

    teste

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ import { Link, useHistory } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchImage from '../images/searchIcon.svg';
 import HeaderSearchBar from './HeaderSearchBar';
+import '../styles/header.css';
 
 function Header({ pageName }) {
   const [isHiddenSearchBar, setSearchBar] = useState(false);
@@ -22,29 +23,40 @@ function Header({ pageName }) {
 
   return (
     <header>
-      <Link
-        src={ profileIcon }
-        data-testid="profile-top-btn"
-        to="/perfil"
-      >
-        <img src={ profileIcon } alt="User" />
-      </Link>
-      <div data-testid="page-title">{ pageName }</div>
-      {toRenderSearchIcon ? (
-        <button
-          type="button"
-          onClick={ () => toggleSearchBar() }
-        >
-          <img
-            src={ searchImage }
-            alt="Search"
-            data-testid="search-top-btn"
-          />
-        </button>
-      ) : (
-        null
-      )}
-      {isHiddenSearchBar ? <HeaderSearchBar /> : null}
+      <div className="border div-header">
+        <div className="btn-profile">
+          <Link
+            className="btn btn-danger"
+            src={ profileIcon }
+            data-testid="profile-top-btn"
+            to="/perfil"
+            style={ { width: 'auto' } }
+          >
+            <img src={ profileIcon } alt="User" />
+          </Link>
+        </div>
+
+        <div data-testid="page-title" className="text-header">{ pageName }</div>
+        <div>
+          {toRenderSearchIcon ? (
+            <button
+              type="button"
+              onClick={ () => toggleSearchBar() }
+              className="btn btn-danger btn-search"
+            >
+              <img
+                src={ searchImage }
+                alt="Search"
+                data-testid="search-top-btn"
+              />
+            </button>
+          ) : (
+            null
+          )}
+          {isHiddenSearchBar ? <HeaderSearchBar /> : null}
+        </div>
+
+      </div>
     </header>
   );
 }
